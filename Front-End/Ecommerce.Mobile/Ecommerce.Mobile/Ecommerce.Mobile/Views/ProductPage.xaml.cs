1ed864f14cb0f0b04c5710a90ec08e539ddda6f9
Juan Hiciano <juanlhiciano@gmail.com>
2021-04-30 22:18:54 -0400

    #14 - Design
    Disable top bar product page

--- Front-End/Ecommerce.Mobile/Ecommerce.Mobile/Ecommerce.Mobile/Views/ProductPage.xaml.cs
+++ Front-End/Ecommerce.Mobile/Ecommerce.Mobile/Ecommerce.Mobile/Views/ProductPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Ecommerce.Mobile.Views
 {
@@ -8,5 +9,14 @@ namespace Ecommerce.Mobile.Views
         {
             InitializeComponent();
         }
+
+        private void MasterDetailButton_Pressed(object sender, EventArgs e)
+        {
+            (App.Current.MainPage as MasterDetailPage).IsPresented = true;
+            //open the master detail page when button is clicked.
+            //MasterDetailPage.IsPresentedProperty.Equals(true);
+            //MenuNavigation.IsPresentedProperty.Equals(true);
+        }
     }
 }
+
