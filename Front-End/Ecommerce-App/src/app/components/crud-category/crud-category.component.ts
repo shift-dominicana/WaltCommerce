b5022ce126f7e396331e90d81ffb8d0b10b2f25f
jmlemoine <jeanmelvinlp27@gmail.com>
2021-06-17 11:01:39 -0400

    #22 - Continue Adding Category

--- Front-End/Ecommerce-App/src/app/components/crud-category/crud-category.component.ts
+++ Front-End/Ecommerce-App/src/app/components/crud-category/crud-category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/category/category.service';
 import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { Category } from 'src/app/models/Category';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-crud-category',
@@ -13,7 +13,9 @@ export class CrudCategoryComponent implements OnInit {
   CategoryList: any[] = []
   closeModal: string;
   editProfileForm: FormGroup;
-  message:string;
+  addProfileForm: FormGroup
+  message: string;
+  closeResult: string;
   constructor(
     private fb: FormBuilder,
     public categoryService: CategoryService,
@@ -26,7 +28,12 @@ export class CrudCategoryComponent implements OnInit {
       id: [''],
       identificator: [''],
       description: ['']
-     });
+    });
+    this.addProfileForm = this.fb.group({
+      id: [''],
+      identificator: [''],
+      description: ['']
+    })
   }
 
   getCategories() {
@@ -52,6 +59,10 @@ export class CrudCategoryComponent implements OnInit {
     );
   }
 
+  openAddModal(content) {
+    this.modalService.open(content)
+  }
+
   openModal(content, category) {
     
     this.modalService.open(content, {
@@ -59,27 +70,78 @@ export class CrudCategoryComponent implements OnInit {
       backdrop: 'static',
       size:'xl',
       ariaLabelledBy: 'modal-basic-title'
-     });
+    })
+      .result.then((result) => {
+        this.closeResult = `Closed with: ${result}`
+        window.location.reload()
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`
+        window.location.reload()
+      })
+    
+    if (this.categoryService.selectedCategory.Id === null) {
+      //console.log("New")
+      //this.openModal(this.addProfileForm, Category)
+      this.editProfileForm.patchValue({
+        id: 0,
+        identificator: "",
+        description: ""
+      });
+    }
+    else {
+      //this.onSubmit()
+      this.editProfileForm.patchValue({
+        id: category.id,
+        identificator: category.identificator,
+        description: category.description
+      });
+    }
+  }
 
-    this.editProfileForm.patchValue({
-      id: category.id,
-      identificator: category.identificator,
-      description: category.description
-    });
+  newCategory = {
+    id: 0,
+    identificator: "",
+    descripton: ""
+  }
+
+  addCategory() {
+    console.log("Klk", this.addProfileForm.value)
+    this.modalService.dismissAll();
+    window.location.reload()
   }
   
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
+      //window.location.reload()
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
+      //window.location.reload()
     } else {
-      return  `with: ${reason}`;
+      return `with: ${reason}`;
+      //window.location.reload()
     }
+  }
+
+  onSaveCategory() {
+    if (this.categoryService.selectedCategory.Id === null) {
+      //console.log("New")
+      this.openModal(this.addProfileForm, Category)
+    }
+    else {
+      //console.log("Update")
+      this.onSubmit()
+    }
+  }
+
+  closeModalApp() {
+    this.modalService.dismissAll();
+    window.location.reload()
   }
 
   onSubmit() {
     this.modalService.dismissAll();
     this.updateCategory(this.editProfileForm.getRawValue())
+    window.location.reload()
    }
 }
